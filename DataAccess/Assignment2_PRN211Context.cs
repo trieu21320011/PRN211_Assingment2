976e9e3155674dd03a57b2164d539ec65dceb306
Trieu <vokhactrieu91@gmail.com>
2022-11-06 20:43:20 +0700

    trieu push context

--- DataAccess/Assignment2_PRN211Context.cs
+++ DataAccess/Assignment2_PRN211Context.cs
@@ -27,7 +27,7 @@ namespace DataAccess
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=Triệu;uid=trieu;pwd=2;database=Assignment2_PRN211");
+                optionsBuilder.UseSqlServer("Server=18.136.108.204;uid=admin;pwd=Nguyenduy111;database=Assignment2_PRN211");
             }
         }
 
